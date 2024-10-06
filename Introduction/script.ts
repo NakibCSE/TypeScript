ffa7a9db5ac99b08258bfaec9ee06b264e6f530d
Nakib <nakkibcse@gmail.com>
2024-10-06 10:53:50 +0600

    Added demonstration of advantage with TS over JS

---
+++ Introduction/script.ts
@@ -0,0 +1,10 @@
+//Demonstration of advantages with TS over JS
+
+//Type safety
+var marks = 10;
+marks = "Nakib";  //It's immediately giving error, so we can't assign a string to a number variable: this is what should be from a programmer prespective.
+marks = 1000;     // This is fine
+console.log(marks); 
+
+
+//Well syntex checking
