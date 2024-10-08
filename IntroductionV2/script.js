61ed82658bbd36610eba87b20957351872a2e252
Nakib <nakkibcse@gmail.com>
2024-10-08 15:54:56 +0600

    Practice nounusedparameters

--- IntroductionV2/script.js
+++ IntroductionV2/script.js
@@ -1,6 +1,6 @@
 "use strict";
 console.log("Assalamualikum from TS.");
-function onAdd() {
+function onAdd(n1, n2) {
     let a = 100;
     console.log(a);
 }
--- IntroductionV2/script.ts
+++ IntroductionV2/script.ts
@@ -1,7 +1,7 @@
 console.log("Assalamualikum from TS.");
 
 
-function onAdd()
+function onAdd(n1: number, n2:number)
 {
     let a = 100;
     console.log(a);
