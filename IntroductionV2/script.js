181c82f9fb4bf6976c8186adade5f2408b3908ba
Nakib <nakkibcse@gmail.com>
2024-10-08 16:12:34 +0600

    Practice noimplicitreturns

--- IntroductionV2/script.js
+++ IntroductionV2/script.js
@@ -3,4 +3,10 @@ console.log("Assalamualikum from TS.");
 function onAdd(n1, n2) {
     let a = 100;
     console.log(a);
+    if (a > 100) {
+        return "Good Result";
+    }
+    else {
+        return "Not Good";
+    }
 }
--- IntroductionV2/script.ts
+++ IntroductionV2/script.ts
@@ -5,4 +5,12 @@ function onAdd(n1: number, n2:number)
 {
     let a = 100;
     console.log(a);
+    if(a>100)
+    {
+        return "Good Result";
+    }
+    else
+    {
+        return "Not Good";
+    }
 }
