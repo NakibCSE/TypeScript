abb137003c6ae3f684dc5d0daf8589ca28cc37fa
Nakib <nakkibcse@gmail.com>
2024-10-08 15:50:15 +0600

    Practice ununsedlocals

--- IntroductionV2/script.js
+++ IntroductionV2/script.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log("Assalamualikum from TS.");
+function onAdd() {
+    let a = 100;
+    console.log(a);
+}
--- IntroductionV2/script.ts
+++ IntroductionV2/script.ts
@@ -1,1 +1,8 @@
 console.log("Assalamualikum from TS.");
+
+
+function onAdd()
+{
+    let a = 100;
+    console.log(a);
+}
