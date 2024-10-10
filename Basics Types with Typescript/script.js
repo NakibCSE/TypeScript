051b8bd1f33544225dc5d18526ef8b0a2af64159
Nakib <nakkibcse@gmail.com>
2024-10-10 15:46:33 +0600

    Practice symbol, Implicit vs Explicit type assignment

--- Basics Types with Typescript/script.js
+++ Basics Types with Typescript/script.js
@@ -18,3 +18,16 @@ concat("Nakib ", "Khan");
 //concat(1, "Khan"); //this is not gonna work
 //Boolean type
 let flg = true;
+//Bigint
+let x = 1.7976931348623157e+309 + 1999999999;
+console.log(x);
+//Symbol
+let s1 = Symbol("Key1");
+let s2 = Symbol("key1");
+console.log(s1 === s2);
+//Type assingment : Implicit vs Explicit
+let x1 = 100;
+//x1 = "Hello";           //Not possible, that is it took the x variable as number
+let x2;
+x2 = 100;
+x2 = "This is a new assignment"; //It's valid as the variable x2 got any date type: it is something like let x2: any;
--- Basics Types with Typescript/script.ts
+++ Basics Types with Typescript/script.ts
@@ -21,3 +21,20 @@ concat("Nakib ", "Khan");
 //Boolean type
 let flg: boolean = true;
 
+//Bigint
+let x:number = 1.7976931348623157e+309+1999999999;
+console.log(x);
+
+//Symbol
+let s1 = Symbol("Key1");
+let s2 = Symbol("key1");
+console.log(s1 === s2);
+
+//Type assingment : Implicit vs Explicit
+let x1 = 100;
+//x1 = "Hello";           //Not possible, that is it took the x variable as number
+let x2;
+x2 = 100;
+x2 = "This is a new assignment";    //It's valid as the variable x2 got any date type: it is something like let x2: any;
+
+
