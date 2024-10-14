78682285942fe2abcc46456326216271f7c0473d
Nakib <nakkibcse@gmail.com>
2024-10-14 12:46:09 +0600

    Practice function inside a object

--- Object/script.js
+++ Object/script.js
@@ -3,7 +3,7 @@
 let emp = {
     empName: "Shaheed Afridi",
     companyName: "ABC",
-    salary: 10000
+    salary: 10000,
 };
 //Showing the results
 console.log(emp);
@@ -17,7 +17,7 @@ emp.empName = "Khan Shaheb";
 let emp1 = {
     empName: "Tamim Iqbal",
     companyName: "BAC",
-    salary: 20000
+    salary: 20000,
 };
 //basically it's like the following
 let a = 100; //declaring a variable which is explicitly typed and assigned a vlue
@@ -27,12 +27,11 @@ x = 900; //thn assign value
 //declare a object
 let emp2;
 //assign value to the object: use the radonly keyword to make it readonly, and use ? after the fiel name to make the field optional
-emp2 =
-    {
-        ID: 'EMP-1923',
-        Name: 'Nazmul Khan',
-        Salary: 10000
-    };
+emp2 = {
+    ID: "EMP-1923",
+    Name: "Nazmul Khan",
+    Salary: 10000,
+};
 console.log(emp2);
 //Readonly and optional in object
 let emp3 = {
@@ -43,11 +42,29 @@ let emp3 = {
 //Onject inside a object : Nested object
 let emp4 = {
     Name: "Mufti Menk",
-    ID: 'EMP-1023232',
+    ID: "EMP-1023232",
     Address: {
         Country: "Australia",
         City: "Sydny",
-        Area: "MM"
-    }
+        Area: "MM",
+    },
 };
 console.log(emp4.Address);
+let emp5 = {
+    name: "Nakib",
+    companyName: "Youngone Corporation",
+    ID: "EMP-1023232",
+    Address: {
+        Country: "Bangladesh",
+        City: "Chittagong",
+        Area: "CEPZ",
+    },
+    display: function () {
+        console.log("Assalamualikum, I am " + this.name
+            + ". I work at " +
+            this.companyName +
+            " & I am form " +
+            this.Address.Country);
+    },
+};
+emp5.display();
--- Object/script.ts
+++ Object/script.ts
@@ -1,8 +1,8 @@
 //Object
 let emp = {
-    empName:"Shaheed Afridi",
-    companyName: "ABC",
-    salary: 10000
+  empName: "Shaheed Afridi",
+  companyName: "ABC",
+  salary: 10000,
 };
 
 //Showing the results
@@ -16,66 +16,84 @@ emp.empName = "Khan Shaheb";
 //emp.salary = "Nakib Khan";      //Not possible due to type mismatch
 
 //Declaring object with explicitly adding type with value assignment
-let emp1 : {
-    empName : string;
-    companyName: string;
-    salary : number;
-}  =  {
-    empName : "Tamim Iqbal",
-    companyName : "BAC",
-    salary : 20000
+let emp1: {
+  empName: string;
+  companyName: string;
+  salary: number;
+} = {
+  empName: "Tamim Iqbal",
+  companyName: "BAC",
+  salary: 20000,
 };
 
 //basically it's like the following
-let a : number = 100;   //declaring a variable which is explicitly typed and assigned a vlue
+let a: number = 100; //declaring a variable which is explicitly typed and assigned a vlue
 
 //It can be done in the following way too
-let x : number; //declare first
+let x: number; //declare first
 x = 900; //thn assign value
 
 //declare a object
-let emp2 :
-{
-    ID : string,
-    Name : string,
-    Salary : number,
+let emp2: {
+  ID: string;
+  Name: string;
+  Salary: number;
 };
 
 //assign value to the object: use the radonly keyword to make it readonly, and use ? after the fiel name to make the field optional
-emp2 = 
-{
-    ID : 'EMP-1923',
-    Name : 'Nazmul Khan',
-    Salary : 10000
-}
+emp2 = {
+  ID: "EMP-1923",
+  Name: "Nazmul Khan",
+  Salary: 10000,
+};
 
 console.log(emp2);
 
 //Readonly and optional in object
 
-let emp3 :
-{
-    readonly Name: string;
-    ID: number;
-    JoiningDate?: Date;
-} =
-{
-    Name : "Tamim Uddin",
-    ID : 2323,
-}
+let emp3: {
+  readonly Name: string;
+  ID: number;
+  JoiningDate?: Date;
+} = {
+  Name: "Tamim Uddin",
+  ID: 2323,
+};
 
 //emp3.Name = "Tamim Khan";    //Give error as the name field is readonly
 
 //Onject inside a object : Nested object
-let emp4 =
-{
-    Name : "Mufti Menk",
-    ID: 'EMP-1023232',
-    Address:
-    {
-        Country : "Australia",
-        City: "Sydny",
-        Area: "MM"
-    }
+let emp4 = {
+  Name: "Mufti Menk",
+  ID: "EMP-1023232",
+  Address: {
+    Country: "Australia",
+    City: "Sydny",
+    Area: "MM",
+  },
 };
 console.log(emp4.Address);
+
+let emp5 = {
+  name: "Nakib",
+  companyName: "Youngone Corporation",
+  ID: "EMP-1023232",
+  Address: {
+    Country: "Bangladesh",
+    City: "Chittagong",
+    Area: "CEPZ",
+  },
+  display: function () {
+    console.log(
+      "Assalamualikum, I am " + this.name
+      +". I work at " +
+        this.companyName +
+        " & I am form " +
+        this.Address.Country
+    );
+  },
+};
+
+emp5.display();
+
+
