422a1560cfa917ef80a4dc800a9a823cc7f04497
Nakib <nakkibcse@gmail.com>
2024-10-20 10:44:52 +0600

    Practice tuple

--- LetVarConst/script.ts
+++ LetVarConst/script.ts
@@ -1,27 +1,37 @@
+
 //Magic of let
 
 //Let var
 var a = 10;
-var a = 19; //Giving no error with the same name 
-if(true)
-{
-    //Some business logic
-    var a = 100;
+var a = 19; //Giving no error with the same name
+if (true) {
+  //Some business logic
+  var a = 100;
 }
-console.log(a);    //It prints 100, which is a problem if we want to have separately available value for business logic
+console.log(a); //It prints 100, which is a problem if we want to have separately available value for business logic
 
-//We can solve this issue with let 
+//We can solve this issue with let
 let x = 10;
 //let x = 90;  //Giving error with the same name
-while(true)
-{
-    //Business logic
-    let x = 100;
+while (true) {
+  //Business logic
+  let x = 100;
 }
 
 console.log(x);
 
-//Const 
+//Const
 const c = 100;
 //c = 900;  //Reassign is not possible
 //const c = 19;  //Redeclare is also not possible
+
+
+//Let in details
+
+let l = 10;
+function fun1()
+{
+    let l = 11;
+}
+fun1();
+console.log(l);
---
+++ Tuple/script.js
@@ -0,0 +1,13 @@
+"use strict";
+//Tuples
+let t = [1, 4, 5];
+let x = ["Kahn", "Nakib", "IT"]; //These two are array as they are holding the same type data
+let z = [1, "Nakib", true]; //This is tupe as it is holding different type value
+function display(pair) {
+    console.log(pair[0]);
+    console.log(pair[1]);
+    console.log(pair[0].length);
+    //console.log(pair[1].length);  //Will give error as number does not a property named length
+    pair.push("Khan Shaheb"); // We can also push new elements to the tuple
+}
+display(["Nakib Khan", 1]);
