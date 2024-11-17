8493ecc40c867ebec1c1795c23fcef7d1d72036c
Nakib <nakkibcse@gmail.com>
2024-11-17 09:47:33 +0600

    Practiced method overriding

--- Inheritance/script.js
+++ Inheritance/script.js
@@ -6,11 +6,21 @@ class BankAccount {
         this.accountNumber = accountNumber;
         this.accountBalance = accountBalance;
     }
-    debit() {
-        console.log("Debit called");
+    debit(amount) {
+        console.log("Debit called from parent class..");
+        let updatedBalance = this.accountBalance - amount;
+        if (updatedBalance < 0) {
+            console.log("Not allowed debit..");
+        }
+        else {
+            this.accountBalance = this.accountBalance - amount;
+        }
     }
     credit() {
         console.log("Credit called");
+    }
+    showBalance() {
+        console.log(this.accountBalance);
     }
 }
 //Child class 1
@@ -29,13 +39,31 @@ class BusinessBankAccount extends BankAccount {
         super(accountName, accountNumber, accountBalance);
         this.businessName = businessName;
     }
+    //Overriding
+    debit(amount) {
+        console.log("Debit called from child class..");
+        let updatedBalance = this.accountBalance - amount;
+        if (updatedBalance < -1000) {
+            console.log("Not allowed debit..");
+        }
+        else {
+            this.accountBalance = this.accountBalance - amount;
+        }
+    }
     businessLoan() {
         console.log("Business loan called.");
     }
 }
 let studentAcc1 = new StudentBankAccount("Nakib", 101, 1000, "ABC");
 studentAcc1.credit();
-studentAcc1.debit();
+//studentAcc1.debit();
 studentAcc1.studentLoan();
 let studentAcc2 = new StudentBankAccount("Rakib", 101, 1000, "ABCD");
-let businessAcc1 = new BusinessBankAccount("Khan", "PQR", 1001, 500000000);
+let businessAcc1 = new BusinessBankAccount("Khan", "PQR", 1001, 5000);
+businessAcc1.showBalance();
+businessAcc1.debit(10);
+businessAcc1.showBalance();
+businessAcc1.debit(5900);
+businessAcc1.showBalance();
+businessAcc1.debit(10);
+businessAcc1.showBalance();
