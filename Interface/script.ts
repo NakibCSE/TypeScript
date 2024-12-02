ae8d3aa8b038b76b3bf407a969a37b3bb8d3ed55
Nakib <nakkibcse@gmail.com>
2024-12-02 08:23:41 +0600

    Practice utility type in TS

--- Interface/script.ts
+++ Interface/script.ts
@@ -12,6 +12,10 @@ interface BankAccountV2 extends BankAccount{
   netBanking: () => void;
 }
 
+interface BankStandard {
+  idProof:string;
+  helpline:() => void;
+}
 class StudentBankAccount implements BankAccountV2 {
   accountNumber: number;
   accountName: string;
@@ -42,10 +46,11 @@ class StudentBankAccount implements BankAccountV2 {
   }
 }
 
-class BusinessBankAccount implements BankAccount{
+class BusinessBankAccount implements BankAccount, BankStandard{
     accountNumber: string; 
     accountName: string;
     accountBalance: number;
+    idProof: string = 'BAcc';
 
     constructor(accountNumber : string, accountName:string, accountBalance:number)
     {
@@ -63,6 +68,48 @@ class BusinessBankAccount implements BankAccount{
   {
     console.log("Debit is happening in Student Bank Account."); 
   }
+  helpline(){
+    console.log("Hello form Businessbank account helpline.....");
+  }
 }
 let StudentAcc1 = new StudentBankAccount(101, "Mr. Khan", 1000);
 let BusinessAccount = new BusinessBankAccount("201", "Tamim Agro", 2000);
+
+
+//Interface as type and utility types: Partial, Required, Omit, Pick
+console.log("Practice interface as a type");
+interface Employee{
+  name: string;
+  city: string;
+  skill: string[]; 
+}
+
+let emp1: Employee = {
+  name : "Nakib Khan",
+  city : "Chattogram",
+  skill : ["C#", "Dot Net", "Angular", "Typescript"],
+};
+
+let emp2: Partial<Employee> = {
+  name : "Nakib Khan",
+  city : "Chattogram",
+};
+
+let emp3:Required<Employee> = {
+  name : "Nakib Khan",
+  city : "Chattogram",
+  skill : ["C#", "Dot Net", "Angular", "Typescript"],
+};
+
+
+let emp4: Omit<Employee, "skill"> = {
+  name : "Nakib Khan",
+  city : "Chattogram",
+  //skill : ["C#", "Dot Net", "Angular", "Typescript"],
+};
+
+let emp5: Pick<Employee, "name" | "city">  = {
+  name : "Nakib Khan",
+  city : "Chattogram",
+  //skill : ["C#", "Dot Net", "Angular", "Typescript"],
+};
